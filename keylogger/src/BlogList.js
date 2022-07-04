f350c56ef5886705d5356d6c8afeada0f333e07d
simonwandera <simonwandera12@gmail.com>
2022-07-04 20:39:40 +0300

    That was easy

--- keylogger/src/BlogList.js
+++ keylogger/src/BlogList.js
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 
 const BlogList = ({ state_name, title, blogs }) => {
-    
     return(
         <div className="blog-list">
             <h2>{ title }</h2>
