df77c066b4303622a0a72baaa6eb6ab13b470016
simonwandera <simonwandera12@gmail.com>
2022-07-06 16:48:53 +0300

    More updates

--- keylogger/src/admin/Admin.js
+++ keylogger/src/admin/Admin.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Table from './Table'
+import useFetch from '../useFetch';
 
 const Admin = () => {
+  const{ data, isPending, error} = useFetch('http://localhost:5000/api/keylogs');
   return (
     <div>
-        <h1>Admin Page</h1>
-        <Table data = {data} title = "All the keystrokes are listed below"/>
+        { error && <div> { error} </div> }
+        { isPending && <div>Loading...</div> }
+        {data && <Table data = {data} title = "All the keystrokes are listed below"/>}
     </div>
   )
 }
--- keylogger/src/admin/Table.js
+++ keylogger/src/admin/Table.js
@@ -4,7 +4,7 @@ const Table = ({title, data}) => {
     console.log(data)
   return (
     <div>
-        <h3>Data table goes here</h3>
+        <h3>Keystrokes</h3>
         <h5>{title}</h5>
     </div>
   )
