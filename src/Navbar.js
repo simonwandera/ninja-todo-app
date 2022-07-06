0641f1b1f61cd02c8ebef4f0bdf70efd0b58622a
simonwandera <simonwandera12@gmail.com>
2022-07-06 22:40:59 +0300

    remved slashes to fix routes

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,8 +5,8 @@ const Navbar = () =>{
             <h1>The Dojo Blog</h1>
             <div className="links">
                 <Link to="">Home</Link>
-                <Link to="/create" >New Blog</Link>
-                <Link to="/admin" >Admin</Link>
+                <Link to="create" >New Blog</Link>
+                <Link to="admin" >Admin</Link>
             </div>
         </nav>
     )
